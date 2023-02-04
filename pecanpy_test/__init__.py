a886bd30cdc2e2fcc627baf855a59273feb3fcfe
Zhaochong Yu <89646174+Scrantu@users.noreply.github.com>
2023-02-04 15:30:06 +0800

    Delete pecanpy_test directory

--- pecanpy_test/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Graph objects equipped with random walk transition functions."""
-from .dense_rw import DenseRWGraph
-from .sparse_rw import SparseRWGraph
-
-__all__ = ["DenseRWGraph", "SparseRWGraph"]
